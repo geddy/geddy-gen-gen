edb5fea0f513215749dc1cc22984240b2ec234dd
Ondrej Brinkel <info@anzui.de>
2014-02-05 00:44:28 +0100

    added support for gulp task runner

---
+++ template/gulp/gulpfile.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var path = require('path');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+
+gulp.task('default', function() {
+  // add your code here
+  console.log('This is the default task of this generator.\nIt does nothing yet.');
+});
+
+gulp.task('help', function() {
+  console.log(
+    fs.readFileSync(
+      path.join(__dirname, 'help.txt'),
+      {encoding: 'utf8'}
+    )
+  );
+});
--- template/gulp/index.js
+++ template/gulp/index.js
@@ -1,1 +1,14 @@
-// gulp gulp
+var path = require('path');
+
+module.exports = function(appPath, args) {
+  var exec = require('child_process').exec;
+
+  // start all the grunting
+  var cmd = exec(path.join(__dirname, 'node_modules/gulp/bin/gulp.js ' + args.join(' ')), {
+    cwd: __dirname
+  }, function() {
+
+  });
+  cmd.stdout.pipe(process.stderr);
+  cmd.stderr.pipe(process.stdout);
+}
